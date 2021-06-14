a18f33468626456862a32b81e7b6e977102ac7a9
Balram Singh <singhbalram786@gmail.com>
2021-06-14 22:08:26 +1000

    Create carousel.js

---
+++ javascript-project/js/library/carousel.js
@@ -0,0 +1,20 @@
+export class Carousel {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+    this.currentIndex = 1;
+	this.showSlide(this.currentIndex)
+  }
+  showSlide(n) {
+    var allSlides = document.getElementsByClassName("img");
+    if(n > allSlides.length){n= 1; this.currentIndex=1;}
+    if(n < 1) {n = allSlides.length; this.currentIndex= allSlides.length;}
+    for(let i= 0; i < allSlides.length; i++){
+      allSlides[i].style.display="none";
+    }
+    allSlides[n-1].style.display = "block";
+  }
+  plusSlide(n){
+    this.showSlide(this.currentIndex = this.currentIndex + n);
+  }
+}
