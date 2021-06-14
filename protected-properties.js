a1118d48312f231f05db12ef9e888cc768a10b8f
Balram Singh <singhbalram786@gmail.com>
2021-06-14 20:30:46 +1000

    Create protected-properties.js

---
+++ protected-properties.js
@@ -0,0 +1,20 @@
+class Something {
+  #property;
+
+  constructor(){
+    this.#property = "test";
+  }
+
+  #privateMethod() {
+    return 'hello world';
+  }
+
+  getPrivateMessage() {
+      return this.#property;
+  }
+}
+
+const instance = new Something();
+console.log(instance.property); //=> undefined
+console.log(instance.privateMethod); //=> undefined
+console.log(instance.getPrivateMessage()); //=> test
