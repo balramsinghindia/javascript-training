a375c83028a38ff0162d5270c402096fb92e9fb7
maryciakbuki <85493489+maryciakbuki@users.noreply.github.com>
2021-06-16 11:27:14 +0530

    Add files via upload

---
+++ React-Classes/javascript-training/javascript-project/js/home.js
@@ -0,0 +1,5 @@
+import Carousel from 'library/carousel.js';
+import Modal from 'library/modal.js';
+
+let myCarousel = new Carousel();
+let myModal = new Modal();
---
+++ React-Classes/javascript-training/javascript-project/js/library/modal.js
@@ -0,0 +1,19 @@
+export class Modal {
+  constructor(){
+    
+  }
+  
+  showModal(){
+    document.getElementsByClassName('my-modal')[0].classList.remove('hidden');
+    document.getElementsByClassName('body')[0].classList.add('gray-background');
+  }
+  
+  hideModal(){
+    document.getElementsByClassName('my-modal')[0].classList.add('hidden');
+    document.getElementsByClassName('body')[0].classList.remove('gray-background');
+  }
+  
+  routeFocus(){
+    document.getElementById('close-icon').focus();
+  }
+}
