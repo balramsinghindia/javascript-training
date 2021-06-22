3a512d68a63eebb5fb2dc0d923d066d2f6e34e07
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-06-22 12:22:54 +0530

    added styles for input border-color

--- FORMS/index.js
+++ FORMS/index.js
@@ -15,21 +15,25 @@ function validateName(){
 
     if(getInputValue){
         document.getElementById('err-one').innerHTML="";
+        document.getElementById('firstname').style.borderColor="";
+
 
          for(let i=0 ; i < getInputValue.length ; i++) {
             if((getInputValue[i]<'A'||getInputValue[i]>'Z') && (getInputValue[i]<'a'||getInputValue[i]>'z')){
                 document.getElementById('err-one').innerHTML="FirstName should contain only characters";
-                document.getElementById('firstname').style.borderBlockEndColor="red";
+                document.getElementById('firstname').style.borderColor="red";
                 console.log('running-loop');
             }
         }
         if((getInputValue.length > 24)  ) {
-             document.getElementById('err-one').innerHTML="First-Name should be within 24 characters only";   
+             document.getElementById('err-one').innerHTML="First-Name should be within 24 characters only";  
+             document.getElementById('firstname').style.borderColor="red";
         }
     }
    
     else {
         let getPara = document.getElementById('err-one').innerHTML="First-Name cannot be empty";
+        document.getElementById('firstname').style.borderColor="red";
         console.log('input is null');
         console.log(getPara);
     } 
@@ -44,19 +48,25 @@ function validateNametwo(){
 
     if(getInputValue){
 
+         document.getElementById('err-two').innerHTML="";
+         document.getElementById('lastname').style.borderColor="";
+
          for(let i=0 ; i < getInputValue.length ; i++) {
             if((getInputValue[i]<'A'||getInputValue[i]>'Z') && (getInputValue[i]<'a'||getInputValue[i]>'z')){
-                document.getElementById('err-two').innerHTML="FirstName should contain only characters";
+                document.getElementById('err-two').innerHTML="LastName should contain only characters";
+                document.getElementById('lastname').style.borderColor="red";
                 console.log('running-loop');
             }
         }
         if((getInputValue.length > 24)  ) {
-             document.getElementById('err-two').innerHTML="First-Name should be within 24 characters only";   
+             document.getElementById('err-two').innerHTML="Last-Name should be within 24 characters only";
+             document.getElementById('lastname').style.borderColor="red";   
         }
     }
    
     else {
-        let getPara = document.getElementById('err-two').innerHTML="First-Name cannot be empty";
+        let getPara = document.getElementById('err-two').innerHTML="Last-Name cannot be empty";
+         document.getElementById('lastname').style.borderColor="red";
         console.log(getPara);
     } 
 }
@@ -74,9 +84,13 @@ function validateEmail(){
     if(getInputValue.length > 0)
             {
                 document.getElementById('err-three').innerHTML="";
+                document.getElementById('email').style.borderColor="";
+
             if( !(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(getInputValueString))){
                 console.log('email is validated'); 
                 document.getElementById('err-three').innerHTML="Please Enter a valid Email-address";
+                document.getElementById('email').style.borderColor="red";
+
 
             }
     }
@@ -84,6 +98,8 @@ function validateEmail(){
     else   {
             console.log('in else section');
             document.getElementById('err-three').innerHTML="Please Enter an Email-address";
+            document.getElementById('email').style.borderColor="red";
+
             }
 }
 
@@ -96,20 +112,29 @@ function validatePhone(){
 
             // /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/
             document.getElementById('err-four').innerHTML="";
+            document.getElementById('phone').style.borderColor="";
+
+            
             if( !(/^\d+$/.test(getInputValue)))
             {
                 document.getElementById('err-four').innerHTML="Enter a Valid Phone Number";
+                document.getElementById('phone').style.borderColor="red";
+
                 console.log('email is validated');
             }
 
             else if (getInputValue.length != 10)
             {
                 document.getElementById('err-four').innerHTML="Phone Number Must be of 10 characters";
+                document.getElementById('phone').style.borderColor="red";
+
             }
     }
     
     else   {
             document.getElementById('err-four').innerHTML="Phone Number cannot be Empty";
+            document.getElementById('phone').style.borderColor="red";
+
             }
 }
 
@@ -120,10 +145,14 @@ function validatePassword(){
     if((/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(getInputValue))){
         console.log("password-validated");
         document.getElementById('err-five').innerHTML="";
+        document.getElementById('password').style.borderColor="";
+
     }
 
     else {
         document.getElementById('err-five').innerHTML="Password Must include altleast 8 character ,1-lowercase 1-uppercase 1-special character";
+        document.getElementById('password').style.borderColor="red";
+
     }
 }
 
@@ -135,10 +164,14 @@ function validateConfirmPassword(){
 
     if(getInputValue == password){
         console.log('confirm-password validated');
+        document.getElementById('confirmpassword').style.borderColor="";
+
     }
 
   else {
     document.getElementById('err-six').innerHTML="Confirm Password Must be Same as Password";
+    document.getElementById('confirmpassword').style.borderColor="red";
+
 
   }
 }
