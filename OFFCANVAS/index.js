3ec307ee3aa54b4ca29004af9aa732cde759e0a5
Prakash Ravichandran <prakash.ravichandran@hashouttech.com>
2021-06-22 18:22:56 +0530

    created a Canvas-component for four sides

--- OFFCANVAS/index.js
+++ OFFCANVAS/index.js
@@ -13,3 +13,46 @@ function closeOffCanvas(){
     document.getElementById('canvas-division').style.transform="translateX(-100%)";
     console.log(canvas);
 }
+
+function openOffCanvasTop(){
+    document.getElementById('canvas-division-two').style.visibility="visible";
+    let canvasdivison = document.getElementById('canvas-division-two').style.transform="none";
+    console.log(canvasdivison);
+}
+
+function closeOffCanvasTop(){
+    let canvas = document.getElementById('canvas-division-two').style.visibility="hidden";
+    document.getElementById('canvas-division-two').style.transform="translateY(-100%)";
+    console.log(canvas);
+}
+
+function openOffCanvasRight(){
+    document.getElementById('canvas-division-three').style.visibility="visible";
+    let canvasdivison = document.getElementById('canvas-division-three').style.transform="none";
+    console.log(canvasdivison);
+}
+
+
+function closeOffCanvasRight(){
+    let canvas = document.getElementById('canvas-division-three').style.visibility="hidden";
+    document.getElementById('canvas-division-three').style.transform="translateX(100%)";
+    console.log(canvas);
+}
+
+function openOffCanvasBottom(){
+    document.getElementById('canvas-division-four').style.visibility="visible";
+    let canvasdivison = document.getElementById('canvas-division-four').style.transform="none";
+    console.log(canvasdivison);
+}
+
+function closeOffCanvasBottom(){
+    let canvas = document.getElementById('canvas-division-four').style.visibility="hidden";
+    document.getElementById('canvas-division-four').style.transform="translateY(100%)";
+    console.log(canvas);
+}
+
+
+
+
+
+
